0ab76add4fe9fe0a1fc1117e26caf0f542938fbc
Shadow prime <gning8044@gmail.com>
2026-04-18 09:43:22 +0000

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -1,6 +1,6 @@
 async function unmuteCommand(sock, chatId) {
     await sock.groupSettingUpdate(chatId, 'not_announcement'); // Unmute the group
-    await sock.sendMessage(chatId, { text: 'ད𝑄𝑈𝐸𝐸𝑁 𝑀𝐸𝑅𝐴-𝑀𝐷 has been unmuted.' });
+    await sock.sendMessage(chatId, { text: 'DEV SHADOW-MD has been unmuted.' });
 }
 
 module.exports = unmuteCommand;
